66d7bd40bd71f26a94f0ef27ba7e69b3834bbd05
ddsnowboard <ddsnowboard@gmail.com>
2014-07-22 22:10:14 -0500

    Made progress on gui.
    Updated WillsLib, and made it actually show the database. It won't change, and
    there's no buttons to edit things yet, but it's getting there. First, before
    anything else, I need to make the function that selects the whole row when you
    click on one, like you have in tShirtPicker. Then we can do other stuff. Good day,
    good day.

--- WillsLib.py
+++ WillsLib.py
@@ -3,7 +3,7 @@ import tkinter as tk
 # Put main DB interface functions from tshirtPicker in here. Generalize them, and they'll be useful. 
 # Instead of this 'if params' business, just have a DBget() to get a specific 
 # one and a DBselect to get all of them. 
-# Also, it would be cool to have a table object that is really python-y instead of
+# Also, it would be cool to have a table object that is really pythonic instead of
 # all annoying and sql-y. 
 def DBinsert(connection, table_name, vals):
 	s = 'insert into '+table_name+' VALUES (?'
@@ -12,7 +12,7 @@ def DBinsert(connection, table_name, vals):
 	s+=');'
 	connection.cursor().execute(s, tuple(vals))
 	connection.commit()
-def DBselect(connection, table_name, columns):
+def DBselect(connection, table_name, columns, which):
 	out = []
 	if columns == 'all':
 		columns = ['*']
@@ -21,8 +21,13 @@ def DBselect(connection, table_name, columns):
 			columns[i] = str(j)
 	else:
 		columns = [str(columns)]
-	for i in connection.cursor().execute('select '+', '.join(columns) +' from '+table_name):
-		out.append(i)
+	if which == 'all':
+		for i in connection.cursor().execute('select '+', '.join(columns) +' from '+table_name):
+			out.append(i)
+	else:
+		strings = [i + " = ?" for i in sorted(which.keys())]
+		for i in connection.cursor().execute("select %s from %s WHERE %s" % (', '.join(columns), table_name, ' and '.join(strings)), tuple([i for i in sorted(which.values())])):
+			out.append(i)
 	return out
 def DBcreate(connection, table_name, columns):
 	s = 'create table '+table_name+'('
@@ -30,3 +35,39 @@ def DBcreate(connection, table_name, columns):
 		s+=i
 	s+=');'
 	connection.cursor().execute(s)
+	connection.commit()
+def DBupdate(connection, table_name, set, which):
+	# Set and which will be dictionaries that have the syntax {column: value}\\
+	# Which could also be the string "all"
+	if not set:
+		raise Exception("""You didn't give the right parameters!\nYou need 
+						 to give 2 dictionaries, \"set\" and \"which\", that\n
+						 are in the format {attribute:value}. See the \n
+						 documentation for more details.""")
+	elif not which:
+		raise Exception("""You didn't give the right parameters!\nYou need 
+						 to give 2 dictionaries, \"set\" and \"which\", that\n
+						 are in the format {attribute:value}. See the \n
+						 documentation for more details.\n
+						 If you want to select all and change, use\n
+						 which='all'""")
+	strings = []
+	for i in sorted(set.keys()):
+		strings.append(str(i)+' = ?')
+	if which == 'all':
+		connection.cursor().execute("update "+table_name+" SET "+', '.join(strings),tuple([j for j in sorted(set.values())]))
+		connection.commit()
+	else:
+		params = []
+		for i in sorted(which.keys()):
+			params.append(str(i)+' = ?')
+		connection.cursor().execute("update "+table_name+" SET "+', '.join(strings)+" WHERE "+' and '.join(params),tuple([j for j in sorted(set.values())]+[i for i in sorted(which.values())]))
+	connection.commit()
+def DBdelete(connection, table_name, which):
+	if which == 'all':
+		connection.cursor().execute("delete from %s" % table_name)
+		db.commit()
+		return
+	strings = [i + " = ?" for i in sorted(which.keys())]
+	connection.cursor().execute("delete from "+table_name+" WHERE "+' and '.join(strings),tuple([i for i in sorted(which.values())]))
+	connection.commit()
--- showDB.py
+++ showDB.py
@@ -4,45 +4,52 @@ import WillsLib
 # This is the version of WillsLib as of 7-20-14, commit hash 3f5c782b8523602173ef649cd55452627ae9155e
 # In 2.4, there is iteritems(), in 3 there is just items(). *sigh*
 class DBColumn(tk.Frame):
-	def __init__(self, root, name):
-		tk.Frame.__init__(self, root)
+	def __init__(self, root, name, index):
+		# Add the command to select all the other columns when you click one. 
+		tk.Frame.__init__(self, root, command=
 		self.name = name
+		self.index = index
 		tk.Label(self, text=name).pack()
-		list = tk.ListBox(self.frame)
+		list = tk.Listbox(self)
 		list.pack()
-		self.curselection = self.list.curselection
-		self.config = self.list.config
-		self.insert = self.list.insert
+		self.curselection = list.curselection
+		self.config = list.config
+		self.insert = list.insert
 		# Make methods to put stuff in the list. 
 		# Also, how to I access the tk.Frame methods?
 class DBList(tk.Frame):
 	def __init__(self, root, connection, table_name):
 		self.connection = connection
-		columns = {}
+		self.columns = {}
 		self.table_name = table_name
 		self.column_names = [i[1] for i in self.connection.cursor().execute("pragma table_info(%s)" % self.table_name)]
 		tk.Frame.__init__(self, root)
-		for i, j in enumerate(column_names):
-			columns[j] = (DBColumn(self, i))
-			columns[j].pack(side='left')
-		tk.Frame.pack()
+		for i, j in enumerate(self.column_names):
+			self.columns[j] = (DBColumn(self, j, i))
+			self.columns[j].pack(side='left')
+		self.populate()
 	def add(self, cols):
 		# Cols is a dictionary.
-		if len(columns) != len(cols):
+		if len(self.columns) != len(cols):
 			raise Exception("You didn't give the right amount of columns!")
 		else:
-			for i, j in items(cols):
-				columns[i].insert('end', j)
+			for i, j in cols.items():
+				self.columns[i].insert('end', j)
 	def populate(self):
 		rows = WillsLib.DBselect(self.connection, self.table_name, 'all', 'all')
 		row_dictionary = {}
 		for i, j in enumerate(rows):
-			self.add({column_names[h]:k for h, k in enumerate(j)})
+			self.add({self.column_names[h]:k for h, k in enumerate(j)})
 def showDB(db_location, table_name):
+	global DB
 	db = sqlite3.connect(db_location)
 	c = db.cursor()
 	root = tk.Tk()
-	DBList(root, db, table_name)
+	DB = DBList(root, db, table_name)
+	DB.pack()
+	root.mainloop()
 	
-if True: # Change this to be if it's opened as it's own program, so it doesn't run if imported. 
+if True: # Change this to be if it's opened as it's own program, so it doesn't run if imported.
+	db = sqlite3.connect('test.db')
+	c = db.cursor()
 	showDB("test.db", 'test')
