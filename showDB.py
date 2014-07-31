3376fa33e81822d341d50c16e3f79dfa1b906805
ddsnowboard <ddsnowboard@gmail.com>
2014-07-30 22:33:36 -0400

    Finished add button.
    2 more to go, and also, I should probably make the columns always go in
    alphabetical order or something, they look weird without doing that.

--- showDB.py
+++ showDB.py
@@ -15,18 +15,40 @@ def switchColumns(base):
 			f.write(i)
 		base.destroy()
 		showDB(sqlite3.location, sqlite3.table_name)
+class addWindow(tk.Tk):
+	def __init__(self, base_list):
+		tk.Tk.__init__(self)
+		self.base_list = base_list
+		self.frames = []
+		self.boxes = []
+		c = self.base_list.connection.cursor()
+		c.execute('pragma table_info(%s)' % sqlite3.table_name)
+		for i in [i[1] for i in c.fetchall()]:
+			self.frames.append(tk.Frame(self))
+			tk.Label(self.frames[-1], text=str(i)+': ').pack(side='left')
+			self.boxes.append(tk.Entry(self.frames[-1]))
+			self.boxes[-1].pack(side='left')
+			self.frames[-1].pack()
+		tk.Button(self, text='OK', command=self.add).pack()
+	def add(self):
+		WillsLib.DBinsert(self.base_list.connection, sqlite3.table_name, [i.get() for i in self.boxes])
+		self.base_list.populate()
+		self.destroy()
 class addButton(tk.Button):
 	def __init__(self, root):
-		tk.Button(self, root, text='Add', command=self.add)
+		tk.Button.__init__(self, root, text='Add', command=self.add)
+		self.root = root
+		self.pack(side='left')
+	def add(self):
+		addWindow(self.root.root)
 # pack left, finish delete and edit buttons. Use WillsLib!
 class buttonBox(tk.Frame):
 	def __init__(self, root, connection):
 		tk.Frame.__init__(self, root)
+		self.root = root
 		self.add = addButton(self)
-		self.edit = editButton(self)
-		self.delete = deleteButton(self)
-		for i in [self.add, self.edit, self.delete]:
-			i.pack()
+		# self.edit = editButton(self)
+		# self.delete = deleteButton(self)
 	def activate(self):
 		for i in [self.edit, self.delete]:
 			i.config(state="normal")		
@@ -45,7 +67,8 @@ class DBColumn(tk.Frame):
 	def select(self, event):
 		selection = self.list.curselection()
 		if selection:
-			root.buttons.activate()
+			# This line will activate the buttons when something is clicked, but it's not done yet. 
+			# root.buttons.activate()
 			for i, j in self.root.columns.items():
 				if not i == self.name:
 					j.highlight(selection[0])
@@ -60,13 +83,15 @@ class DBColumn(tk.Frame):
 	
 class DBList(tk.Frame):
 	def __init__(self, root, connection, table_name, cols):
+		tk.Frame.__init__(self, root)
 		self.connection = connection
 		self.columns = {}
 		self.table_name = table_name
 		self.column_names = cols
 		self.switch_button = tk.Button(root, text='Switch columns', command=lambda:switchColumns(root))
+		self.button_box = buttonBox(self, self.connection)
+		self.button_box.pack()
 		self.switch_button.pack()
-		tk.Frame.__init__(self, root, command=self.click)
 		self.scrollbar = tk.Scrollbar(self, orient = 'vertical', command = self.scroll)
 		for i, j in enumerate(self.column_names):
 			self.columns[j] = (DBColumn(self, j))
@@ -91,6 +116,8 @@ class DBList(tk.Frame):
 	def populate(self):
 		rows = WillsLib.DBselect(self.connection, self.table_name, self.column_names, 'all')
 		row_dictionary = {}
+		for i in self.columns.values():
+			i.list.delete(0, 'end')
 		for i, j in enumerate(rows):
 			self.add({self.column_names[h]:k for h, k in enumerate(j)})
 def closeCols(picked_columns, column_picker, db, table_name, write):
